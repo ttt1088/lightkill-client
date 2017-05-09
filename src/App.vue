85381063e95f38b610fe0414eac1b5e758bbb74e
ttt1088 <yaoyaoyuxiaoxiao@163.com>
2017-05-09 21:20:12 +0800

    5.9

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <mu-appbar title="Title">
+    <mu-appbar title="LightKill">
     </mu-appbar>
     <div class="content">
       <mu-text-field label="名字" labelFloat v-model="username" /><br/>
       <mu-text-field label="密码" type="password" labelFloat v-model="password"/><br/>
       <mu-raised-button label="注册" fullWidth @click="reg" primary />
     </div>
+    <mu-dialog :open="dialog" title="提示">
+      {{ dialogMessage }}
+    <mu-flat-button label="确定" slot="actions" primary @click="dialogClose"/>
+    </mu-dialog>
   </div>
 </template>
 
@@ -17,15 +21,39 @@ export default {
   data() {
     return{
       numbers: [ 1, 2, 3, 4, 5 ],
-      username:null,
-      password:null
+      username:'',
+      password:'',
+      dialog: false,
+      dialogMessage:''
   }},
   computed: {
 
   },
   methods: {
     reg: function(){
-      this.socket.emit('reg', { name: this.username, password: this.password });
+      const that = this;
+      this.socket.emit('reg', { name: this.username, password: this.password }, function(data) {
+        console.log(data);
+        if(data.errno == 0){
+          that.dialog = true;
+          that.dialogMessage = '注册成功';
+        }
+        else if(data.errno == 1){
+          that.dialog = true;
+          that.dialogMessage = '注册失败：未知错误';
+        }
+        else if(data.errno == 2){
+          that.dialog = true;
+          that.dialogMessage = '注册失败：用户名已存在';
+        }
+        else if(data.errno == 3){
+          that.dialog = true;
+          that.dialogMessage = '注册失败：用户名或密码格式错误';
+        }
+      });
+    },
+    dialogClose: function(){
+      this.dialog = false;
     }
   },
   created() {
